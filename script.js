b38fec056df303e4ada46570e695680b022c119a
talarkn <talar.kijilian19@gmail.com>
2023-03-19 13:18:05 +0100

    Create operate function which takes an operator and 2 numbers and then calls one of the previously created functions on the numbers.

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    if (operator === '+') add(num1, num2);
+    if (operator === '-') substract(num1, num2);
+    if (operator === '*') multiply(num1, num2);
+    if (operator === '/') divide(num1, num2);
+}
