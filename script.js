44233bb1804139d88012ea550769fdce3418357e
talarkn <talar.kijilian19@gmail.com>
2023-04-24 21:35:02 +0200

    Remove cleanNumber function from num1 and num2. Use instead the toString and slice methods to display max 9 digits. Set operator to null within the getOperator function if the '=' button is clicked. Now once there is a solution the % and +/- buttons will apply on the solution.

--- script.js
+++ script.js
@@ -37,23 +37,23 @@ operationDisplay.textContent = 0;
 
 function getNumber(event) {
     if (!operator) {
-        num1 = addDigit(event, num1);
-        operationDisplay.textContent = cleanNumber(num1);
+        num1 = addDigit(event, num1).toString().slice(0, 9);
+        operationDisplay.textContent = num1;
     }
     else {
-        num2 = addDigit(event, num2);
-        operationDisplay.textContent = cleanNumber(num2);
+        num2 = addDigit(event, num2).toString().slice(0, 9);
+        operationDisplay.textContent = num2;
     }
     decimal.value = '.';
 }
-// % or +/- applies after calculation is resulting in empty string -> check num2
+
 function addDigit(event, num) {
     if (num.toString().includes('.')) decimal.value = '';
     else if (num === '' && event.target.value === '.') num = 0;
 
-    //if (event.target.value === '%') num *= 0.01; 
-    //else if (event.target.value === '+/-') num *= -1;
-    num += event.target.value;
+    if (event.target.value === '%') num *= 0.01; 
+    else if (event.target.value === '+/-') num *= -1;
+    else num += event.target.value;
 
     return num === 0 ? '' : num;
 }
@@ -61,7 +61,10 @@ function addDigit(event, num) {
 function getOperator(event) {
     if (num2) solution = operate(operator, Number(num1), Number(num2));
     else solution = num1;
+
     if (event.target.value !== '=') operator = event.target.value;
+    else operator = '';
+    
     num1 = solution;
     num2 = '';
     operationDisplay.textContent = cleanNumber(solution);
